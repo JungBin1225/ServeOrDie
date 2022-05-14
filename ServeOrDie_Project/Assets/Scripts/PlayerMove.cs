75e66532117e774f0b4b27d19e2783391d0aab1a
smatphone <jb5982@naver.com>
2022-05-14 23:45:15 +0900

    캐릭터 선정 및 이동 추가
    1. 캐릭터 선정 및 애니메이션 제작
    
    2. 애니메이션 적용
    
    3. 캐릭터 이동 스크립트 추가

---
+++ ServeOrDie_Project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rigidbody;
+    private Vector3 mousePos;
+
+    public float speed;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        mousePos = UpdateMousePos();
+
+        if(Mathf.Abs(mousePos.x) > Mathf.Abs(mousePos.y))
+        {
+            if(mousePos.x > 0)
+            {
+                anim.SetInteger("direction", 2);
+            }
+            else
+            {
+                anim.SetInteger("direction", 4);
+            }
+        }
+        else
+        {
+            if(mousePos.y > 0)
+            {
+                anim.SetInteger("direction", 1);
+            }
+            else
+            {
+                anim.SetInteger("direction", 3);
+            }
+        }
+
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+
+        Vector2 getVel = new Vector2(xMove, yMove) * speed;
+        rigidbody.velocity = getVel;
+
+        if(rigidbody.velocity.magnitude == 0)
+        {
+            anim.SetBool("isMove", false);
+        }
+        else
+        {
+            anim.SetBool("isMove", true);
+        }
+    }
+
+    private Vector3 UpdateMousePos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
